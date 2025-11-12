945bf263d0710034058271417b49545c5a066b91
TaeEun Ham <taeng040108@gamil.com>
2025-11-12 16:41:38 +0900

    Assignment solved

---
+++ tester.py
@@ -0,0 +1,10 @@
+import geo.utils as utils
+
+#calculate the length of hypotenuse(c) when a = 3 and b = 4
+a, b = 3, 4
+c = utils.pythagoras(3, 4)
+print('c = ', c)
+
+r = 10
+area = utils.circle(10)
+print('area =', area)
